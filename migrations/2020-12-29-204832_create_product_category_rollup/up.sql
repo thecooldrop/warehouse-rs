36f0f4d6dbbfd85cffd1c480452129535f204090
Vanio Begic <vanio.begic123@gmail.com>
2020-12-29 21:53:45 +0100

    Create product category rollup entity

---
+++ migrations/2020-12-29-204832_create_product_category_rollup/up.sql
@@ -0,0 +1,7 @@
+create table product_category_rollup(
+    id serial,
+    upper_category_id integer not null,
+    lower_category_id integer not null,
+    foreign key (upper_category_id) references product_category(id),
+    foreign key (lower_category_id) references product_category(id)
+);
