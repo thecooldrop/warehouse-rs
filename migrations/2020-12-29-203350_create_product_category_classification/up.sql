5c507efe31ad4cc4922db929bd0b715298131edd
Vanio Begic <vanio.begic123@gmail.com>
2020-12-29 21:47:54 +0100

    Fix foreign key type for create_product_category_classification migration

--- migrations/2020-12-29-203350_create_product_category_classification/up.sql
+++ migrations/2020-12-29-203350_create_product_category_classification/up.sql
@@ -1,7 +1,7 @@
 create table product_category_classification (
     id serial,
-    product_id serial not null,
-    product_category_id serial not null,
+    product_id integer not null,
+    product_category_id integer not null,
     primary key (id),
     foreign key (product_id) references product(id),
     foreign key (product_category_id) references product_category(id)
