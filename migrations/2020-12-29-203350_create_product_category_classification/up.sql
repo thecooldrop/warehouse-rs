48316e97a2af48ba68656006e464e69f03198cf0
Vanio Begic <vanio.begic123@gmail.com>
2020-12-29 21:53:05 +0100

    Add is_primary field to product category classification

--- migrations/2020-12-29-203350_create_product_category_classification/up.sql
+++ migrations/2020-12-29-203350_create_product_category_classification/up.sql
@@ -2,6 +2,7 @@ create table product_category_classification (
     id serial,
     product_id integer not null,
     product_category_id integer not null,
+    is_primary_classification boolean not null,
     primary key (id),
     foreign key (product_id) references product(id),
     foreign key (product_category_id) references product_category(id)
