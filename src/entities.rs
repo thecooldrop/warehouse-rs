5ef3ce8a7290b50364c1a1171f64872b471a80d6
Vanio Begic <vanio.begic123@gmail.com>
2021-01-06 15:56:33 +0100

    Revert back to join-semantic for resolving many-to-many relationships

--- src/entities.rs
+++ src/entities.rs
@@ -26,16 +26,8 @@ pub struct ProductCategory {
     name: String,
 }
 
-#[derive(Identifiable, Queryable)]
-#[table_name = "product_category"]
-pub struct UpperProductCategory {
-    pub id: i32,
-    pub name: String,
-}
 
 #[derive(Queryable, Identifiable, Associations)]
-#[belongs_to(UpperProductCategory, foreign_key="upper_category_id")]
-#[belongs_to(ProductCategory, foreign_key="lower_category_id")]
 #[table_name = "product_category_rollup"]
 pub struct ProductCategoryRollup {
     id: i32,
@@ -49,13 +41,4 @@ pub struct InventoryItem {
     id: i32,
     product_id: i32,
     instance_description: String,
-}
-
-impl From<UpperProductCategory> for ProductCategory {
-    fn from(upper_category: UpperProductCategory) -> Self {
-        ProductCategory {
-            id: upper_category.id,
-            name: upper_category.name
-        }
-    }
 }
