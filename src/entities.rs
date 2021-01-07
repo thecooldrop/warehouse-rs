9f69e93d9fb9cb9a71e64dc517ae301fd15fb8d4
Vanio Begic <vanio.begic123@gmail.com>
2021-01-07 21:59:53 +0100

    Add documentation for basic structs

--- src/entities.rs
+++ src/entities.rs
@@ -3,15 +3,25 @@ use super::schema::*;
 
 #[derive(Queryable, Identifiable)]
 #[table_name = "product"]
+/// A product which is sold by the company
+///
+/// The product represents only the description of product. For example "A Book - FirstName Last"
+/// represents only that we are aware of this product, while separate entities are used to represent
+/// other information, such as how much stock of this product do we have, or which competitor too
+/// sells this product.
 pub struct Product {
     id: i32,
     description: String,
 }
 
+
 #[derive(Queryable, Identifiable, Associations)]
 #[belongs_to(Product, foreign_key = "product_id")]
 #[belongs_to(ProductCategory, foreign_key = "product_category_id")]
 #[table_name = "product_category_classification"]
+/// Classification of products into categories
+///
+/// Specifies to which categories does a product belong.
 pub struct ProductCategoryClassification {
     id: i32,
     product_id: i32,
@@ -21,6 +31,10 @@ pub struct ProductCategoryClassification {
 
 #[derive(Identifiable, Queryable)]
 #[table_name = "product_category"]
+/// Product category
+///
+/// Represents possible grouping of products. Examples of products would be "Books", "Clothes"
+/// or "Office supplies"
 pub struct ProductCategory {
     id: i32,
     name: String,
@@ -29,6 +43,14 @@ pub struct ProductCategory {
 
 #[derive(Queryable, Identifiable, Associations)]
 #[table_name = "product_category_rollup"]
+/// Hierarchy of product categories
+///
+/// Represents how categories can consists of other subcategories. For example "Clothes" category
+/// can contain "Bikinis", "Shirts", "Swimwear" as subcategories. One category can have many parent
+/// categories.
+///
+/// Note category should not be related to children of its children. Basically the relationship of
+/// categories should not be transitive.
 pub struct ProductCategoryRollup {
     id: i32,
     upper_category_id: i32,
@@ -37,6 +59,14 @@ pub struct ProductCategoryRollup {
 
 #[derive(Queryable, Identifiable)]
 #[table_name = "inventory_item"]
+/// Instance of product
+///
+/// Represents an instance of product. For example if we have product "A" which is a book, then we
+/// can have 10 inventory items, which are product "A", basically we can have 10 copies of book in
+/// stock.
+///
+/// Note currently this type represents a serializable product. This means that each instance of a
+/// product is considered to be unique. There is currently no concept of "lot" or "stack" of product
 pub struct InventoryItem {
     id: i32,
     product_id: i32,
