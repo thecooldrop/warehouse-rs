e3621010a55b4046b913a0681da22bfa49853184
Vanio Begic <vanio.begic123@gmail.com>
2021-01-30 20:43:48 +0100

    Move controllers into library module

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
 #[macro_use]
 extern crate diesel;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate diesel_migrations;
 
-pub mod entities;
+mod entities;
 pub mod schema;
+pub mod controllers;
+
+#[database("pgdatabase")]
+pub struct DbConn(diesel::PgConnection);
 
 #[cfg(test)]
 mod tests {
